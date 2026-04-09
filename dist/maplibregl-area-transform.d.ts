15b32a16a2e48256c7525afbf2c802baeefd4eaa
HarelM <HarelM@users.noreply.github.com>
2026-04-09 13:32:09 +0000

    deploy docs 35c2d1082248e1d147b1d00288602633d4d0323d

--- dist/maplibregl-area-transform.d.ts
+++ dist/maplibregl-area-transform.d.ts
@@ -2,6 +2,7 @@ import { IControl, Map } from 'maplibre-gl';
 
 type MaplibreAreaTransformOptions = {
     showAddImageButton?: boolean;
+    showAddRectangleButton?: boolean;
 };
 type Corners = [[number, number], [number, number], [number, number], [number, number]];
 declare class MaplibreAreaTransform implements IControl {
@@ -9,33 +10,50 @@ declare class MaplibreAreaTransform implements IControl {
     private _map;
     private _container;
     private _eventEmitter;
-    private _maxImageId;
-    private _selectedImageId;
+    private _selectedFeatureId;
     private _isScaling;
     private _isRotating;
+    private _isAddingRectangle;
+    private _rectanglePoints;
     private _startPoint;
     private _startPointCoordinates;
     constructor(options?: MaplibreAreaTransformOptions);
     /** @inheritdoc */
     onAdd(map: Map): HTMLElement;
     private initFileButton;
+    private initRectangleButton;
+    private initGeojsonSourceAndLayers;
     /** @inheritdoc */
     onRemove(): void;
-    addImage(imageUrl: string, coordinates: Corners): string;
+    addImage(imageUrl: string, coordinates: Corners): Promise<string>;
+    startAddRectangleSequence(): void;
+    private onClickAddRectanglePoint;
+    /**
+     * Sort corners of a rectangle in clockwise order starting from the top-left corner.
+     * @param corners The corners of the rectangle.
+     * @returns The sorted corners of the rectangle.
+     */
+    private sortCorners;
+    addRectangle(coordinates: Corners): Promise<void>;
     on(event: string, listener: (...args: any[]) => void): void;
     off(event: string, listener: (...args: any[]) => void): void;
     private onFileSelected;
     private getOpositePoint;
     private getCenter;
     private initMapListeners;
-    private buildGeoJSON;
+    private buildPolygonGeoJSONFeatures;
     private getRotateHandlePoint;
     private getScaleHandleHeading;
     private onMouseMoveForCursor;
     private onMouseDown;
+    private setStateFromMouseDown;
     private onMouseMove;
     private onMouseUp;
+    private onClick;
     private initImages;
+    private removeSelection;
+    private setSelection;
+    private updateCoordinates;
 }
 
 export { MaplibreAreaTransform };
