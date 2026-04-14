5064bf7d2edb91792ff0fc6b48b19793705d4d19
HarelM <HarelM@users.noreply.github.com>
2026-04-14 07:48:01 +0000

    deploy docs b38e7f40ce8443e4cc5cc5928fd6b6b3ada200eb

--- dist/maplibregl-area-transform.d.ts
+++ dist/maplibregl-area-transform.d.ts
@@ -1,10 +1,38 @@
 import { IControl, Map } from 'maplibre-gl';
 
+/**
+ * Options for the maplibre area transform control
+ */
 type MaplibreAreaTransformOptions = {
+    /**
+     * Show the add image button
+     * @default true
+     */
     showAddImageButton?: boolean;
+    /**
+     * Show the polygon button
+     * @default true
+     */
+    showPolygonButton?: boolean;
+    /**
+     * Show the add rectangle button
+     * @default true
+     */
     showAddRectangleButton?: boolean;
 };
-type Corners = [[number, number], [number, number], [number, number], [number, number]];
+/**
+ * Maplibre area transform control
+ *
+ * @example
+ * ```typescript
+ * const map = new Map({
+ *   container: 'map',
+ *   style: 'https://demotiles.maplibre.org/style.json',
+ * });
+ * const areaTransform = new MaplibreAreaTransform();
+ * map.addControl(areaTransform);
+ * ```
+ */
 declare class MaplibreAreaTransform implements IControl {
     private options;
     private _map;
@@ -13,20 +41,32 @@ declare class MaplibreAreaTransform implements IControl {
     private _selectedFeatureId;
     private _isScaling;
     private _isRotating;
-    private _isAddingRectangle;
-    private _rectanglePoints;
+    private _isAddingPolygon;
+    private _isResizing;
+    private _polygonPoints;
     private _startPoint;
     private _startPointCoordinates;
     constructor(options?: MaplibreAreaTransformOptions);
     /** @inheritdoc */
     onAdd(map: Map): HTMLElement;
+    /**
+     * Initialize the file button
+     */
     private initFileButton;
+    /**
+     * Initialize the polygon button
+     */
+    private initPolygonButton;
+    /**
+     * Initialize the rectangle button
+     */
     private initRectangleButton;
     private initGeojsonSourceAndLayers;
     /** @inheritdoc */
     onRemove(): void;
-    addImage(imageUrl: string, coordinates: Corners): Promise<string>;
-    startAddRectangleSequence(): void;
+    addImage(imageUrl: string, coordinates: GeoJSON.Position[]): Promise<string>;
+    onClickAddRectangle: () => void;
+    startAddPolygonSequence(): void;
     private onClickAddRectanglePoint;
     /**
      * Sort corners of a rectangle in clockwise order starting from the top-left corner.
@@ -34,7 +74,7 @@ declare class MaplibreAreaTransform implements IControl {
      * @returns The sorted corners of the rectangle.
      */
     private sortCorners;
-    addRectangle(coordinates: Corners): Promise<void>;
+    addPolygon(coordinates: GeoJSON.Position[], resizable: boolean): Promise<void>;
     on(event: string, listener: (...args: any[]) => void): void;
     off(event: string, listener: (...args: any[]) => void): void;
     private onFileSelected;
@@ -43,6 +83,7 @@ declare class MaplibreAreaTransform implements IControl {
     private initMapListeners;
     private buildPolygonGeoJSONFeatures;
     private getRotateHandlePoint;
+    private getResizeHandlePoints;
     private getScaleHandleHeading;
     private onMouseMoveForCursor;
     private onMouseDown;
@@ -54,7 +95,12 @@ declare class MaplibreAreaTransform implements IControl {
     private removeSelection;
     private setSelection;
     private updateCoordinates;
+    private rotatePolygon;
+    private scalePolygon;
+    private resizeRectangleSide;
+    private getClosestEdgeIndex;
+    private getMidPoint;
 }
 
 export { MaplibreAreaTransform };
-export type { Corners, MaplibreAreaTransformOptions };
+export type { MaplibreAreaTransformOptions };
